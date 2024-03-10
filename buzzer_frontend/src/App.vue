16fce563142c24c3a710c9e2424c0638a87eb7a0
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2024-03-10 16:23:55 +0100

    Add the buzzer button vue component.

--- buzzer_frontend/src/App.vue
+++ buzzer_frontend/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div
+    class="flex h-screen w-screen flex-wrap place-content-center gap-4 bg-neutral-200"
+  >
+    <BuzzerButton
+      @start-buzzing="console.log('start')"
+      @stop-buzzing="console.log('stop')"
+    ></BuzzerButton>
+  </div>
 </template>
+
+<script setup lang="ts">
+import BuzzerButton from "./components/BuzzerButton.vue";
+
+// const websocket = new WebSocket("ws://" + location.hostname + "/ws");
+// websocket.onopen = function (event) {
+//   console.log("Successfully connected to websocket.", event);
+// };
+</script>
---
+++ buzzer_frontend/src/components/BuzzerButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="size-32 rounded-xl border-2 border-black bg-emerald-700 p-2 lg:size-40"
+    @touchstart.prevent="$emit('startBuzzing')"
+    @touchend.prevent="$emit('stopBuzzing')"
+    @mousedown.prevent="$emit('startBuzzing')"
+    @mouseup.prevent="$emit('stopBuzzing')"
+  ></button>
+</template>
+
+<script setup lang="ts">
+defineEmits(["startBuzzing", "stopBuzzing"]);
+</script>
